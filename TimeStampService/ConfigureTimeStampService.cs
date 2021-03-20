4174cb560b617fdeece10dc1d0f1902e9afa9d1a
dfolga <58554378+dfolga@users.noreply.github.com>
2021-03-20 22:32:14 +0100

    Dodaj pliki projektów.

---
+++ TimeStampService/ConfigureTimeStampService.cs
@@ -0,0 +1,46 @@
+﻿using NLog;
+using System;
+using Topshelf;
+
+namespace TimeStampService
+{
+    class ConfigureTimeStampService
+    {
+        static void Main(string[] args)
+        {
+            var config = new NLog.Config.LoggingConfiguration();
+
+            // Targets where to log to: File and Console
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "logs/file.txt" };
+            var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+
+            // Rules for mapping loggers to targets            
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+            config.AddRule(LogLevel.Debug, LogLevel.Fatal, logfile);
+
+            // Apply config           
+            NLog.LogManager.Configuration = config;
+
+
+            var rc = HostFactory.Run(x =>
+            {
+                x.Service<TimeStampService>(s =>
+                {
+                    s.ConstructUsing(timestampservice => new TimeStampService());
+                    s.WhenStarted(timestampservice => timestampservice.Start());
+                    s.WhenStopped(timestampservice => timestampservice.Stop());
+                });
+                x.UseNLog();
+
+                x.RunAsLocalSystem();
+
+                x.SetServiceName("TimeStampService");
+                x.SetDisplayName("TimeStampService");
+                x.SetDescription("This is a service which is making timestamp files and is logging timestamp using NLog");
+            });
+
+            var exitCode = (int)Convert.ChangeType(rc, rc.GetTypeCode());  
+            Environment.ExitCode = exitCode;
+        }
+    }
+}
---
+++ TimeStampService/TimeStampService.cs
@@ -0,0 +1,55 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace TimeStampService
+{
+    class TimeStampService
+    {
+        private readonly Timer _timer;
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public TimeStampService()
+        {
+            _timer = new Timer();
+            _timer.Interval = 60*1000;
+            _timer.Elapsed += new ElapsedEventHandler(SixtySecondsEvent);
+            _timer.AutoReset = true;
+            _timer.Enabled = true;
+        }
+
+        private static void SixtySecondsEvent(object sender, ElapsedEventArgs e)
+        {
+            string path = Directory.GetCurrentDirectory();
+            string TimeStamp = DateTime.Now.ToString("s");
+            String FormattedTimeStamp = TimeStamp.Replace(":",".");
+            string FileName = Path.Combine(path, FormattedTimeStamp + ".txt");
+            try
+            {
+                File.WriteAllText(FileName, TimeStamp);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                logger.Error("Catched exception:" + ex);
+
+            }
+            logger.Info("Timestamp file successfully created");
+           
+        }
+        public void Start() {
+            _timer.Start();
+            logger.Info("Service started!");
+        }
+        public void Stop()
+        {
+            _timer.Stop();
+            logger.Info("Service stopped!");
+        }
+    }
+}
